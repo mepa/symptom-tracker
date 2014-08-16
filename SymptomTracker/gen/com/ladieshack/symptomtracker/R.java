56c9bbc445eb745e6ee002c190b84bf7d35cc33a
Tania Goswami <tania@lp-3621.ad.datafoundry.com>
2014-08-16 16:12:23 -0500

    Updated new user page to include DOB field and added title to menu page

--- SymptomTracker/gen/com/ladieshack/symptomtracker/R.java
+++ SymptomTracker/gen/com/ladieshack/symptomtracker/R.java
@@ -25,8 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int go_to_main_menu_button=0x7f080005;
+        public static final int action_settings=0x7f080009;
+        public static final int birthday_day_input=0x7f080006;
+        public static final int birthday_month_input=0x7f080005;
+        public static final int birthday_year_input=0x7f080007;
+        public static final int go_to_main_menu_button=0x7f080008;
         public static final int go_to_open_symptom_button=0x7f080002;
         public static final int go_to_report_symptoms_button=0x7f080003;
         public static final int go_to_symptom_history_button=0x7f080001;
@@ -45,24 +48,29 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f05000b;
+        public static final int action_settings=0x7f050010;
         public static final int app_name=0x7f050000;
-        public static final int dob=0x7f050005;
+        public static final int dd=0x7f050008;
+        public static final int dob=0x7f050006;
         public static final int enter_app=0x7f050003;
         public static final int hello_world=0x7f050001;
-        public static final int name=0x7f050004;
         /**  Main menu screen 
          */
-        public static final int new_symptom=0x7f050006;
-        public static final int open_symptom=0x7f050008;
-        public static final int report_symptoms=0x7f050009;
-        public static final int symptom_history=0x7f050007;
+        public static final int menu=0x7f05000a;
+        public static final int mm=0x7f050007;
+        public static final int name=0x7f050004;
+        public static final int name_hint=0x7f050005;
+        public static final int new_symptom=0x7f05000b;
+        public static final int open_symptom=0x7f05000d;
+        public static final int report_symptoms=0x7f05000e;
+        public static final int symptom_history=0x7f05000c;
         /**  Whole body screen 
          */
-        public static final int tell_us_where_hurts=0x7f05000a;
+        public static final int tell_us_where_hurts=0x7f05000f;
         /**  New user screen 
          */
         public static final int we_need_info=0x7f050002;
+        public static final int yyyy=0x7f050009;
     }
     public static final class style {
         /** 
