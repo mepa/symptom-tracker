088f9288184fed651d135060a9cbba96fc41fb7c
jkru <julie.hollek@gmail.com>
2014-08-16 15:56:19 -0500

    latest tracker py file

--- trackermain.py
+++ trackermain.py
@@ -1,5 +1,14 @@
 from sys import argv
 import time
+import datetime
+
+outputfile1 = open('userinfo.sql','w')
+outputfile2 = open('eventinfo.sql','w')
+
+outputfile1.write("BEGIN TRANSACTION;\n")
+outputfile1.write(" ")
+outputfile2.write("BEGIN TRANSACTION;\n")
+outputfile2.write(" ")
 
 script, bodypartf, symptomf = argv
 txt1 = open(bodypartf)
@@ -13,6 +22,7 @@ j = len(eachline2)
 intrinsicappendage = []
 intrinsicsymptom = []
 intrinsicintensity = []
+intrinsictemperature = []
 
 print "What's your name?"
 username = raw_input()
@@ -32,30 +42,69 @@ for b in range (0, j):
 print "Where does it hurt?"
 extrinsicbodypart = raw_input()
 
-starttime = time.time()
-
+starttimecomputer = time.time()
+startdate = datetime.datetime.now().date()
+startdate = str(startdate)
+starttime = datetime.datetime.now().time()
+print starttime
 print "How does it hurt? ache, fever, chills "
 extrinsicsymptom = raw_input() 
 
-if extrinsicsymptom == "fever":
+if extrinsicsymptom == 'fever':
     print "What is your temperature?"
-    extrinsicintensity = raw_input()
+    extrinsictemperature = raw_input()
     for b in range (0, 10):
-        intensity = 96 + float(b) * 10 
-        intrinsicintensity.append(intensity)
+        temperature = 96 + float(b) * 10 
+        intrinsictemperature.append(temperature)
 
-    
-else:
-    print "On a scale of 1-10 how severe is your %s ?" % extrinsicsymptom
-    extrinsicintensity = raw_input()
-    for b in range (0, 10):
-        intensity = b
-        intrinsicintensity.append(intensity)
+
+print "On a scale of 1-10 how severe is your %s ?" % extrinsicsymptom
+extrinsicintensity = raw_input()
+for b in range (0, 10):
+    intensity = b
+    intrinsicintensity.append(intensity)
 
 print "has this ended yet?"
 ended = raw_input()
 if ended == "yes":
-    duration = time.time()-starttime
-print duration," s"
+    duration = time.time()-starttimecomputer
+    endtime = datetime.datetime.now().time()
+#print duration," s"
 
-print "INSERT INTO user (firstname, dob) VALUES ('"+username+"','"+dateofbirth+"')"
+enddate = datetime.datetime.now().date()
+enddate = str(enddate)
+duration = str(duration)
+
+starttime = str(starttime)
+endtime = str(endtime)
+extrinsicintensity = str(extrinsicintensity)
+
+if 'extrinsictemperature' in locals():
+    extrinsictemperature = str(extrinsictemperature)
+else:
+    extrinsictemperature = 98.66
+extrinsictemperature = str(extrinsictemperature)
+
+#print "INSERT INTO user (firstname, dob) VALUES ('"+username+"','"+dateofbirth+"')"
+
+#print "INSERT INTO abundance (user_id, date, start_time, end_time, body_part, symptom, severity, temperature) WHERE user.firstname = '"+username+"' SELECT user_id, "+today+","+starttime+","+endtime+","+extrinsicbodypart+","+extrinsicsymptom+","+extrinsicintensity+","+extrinsictemperature
+
+#print type(starttime)
+#print type(endtime)
+#print type(today)
+#print type(extrinsicintensity)
+#print type(extrinsictemperature)
+#print type(starttime)
+
+
+
+outputfile1.write("INSERT INTO user (firstname, dob) VALUES ('"+username+"','"+dateofbirth+"');")
+
+outputfile2.write("INSERT INTO event (user_id, start_date, start_time, start_time, end_date, end_time, duration, body_part, symptom, intensity, temperature) WHERE user.firstname = '"+username+"' SELECT user_id, "+startdate+","+starttime+","+enddate+","+endtime+","+duration+","+extrinsicbodypart+","+extrinsicsymptom+","+extrinsicintensity+","+extrinsictemperature+";")
+
+
+outputfile1.write("\nCOMMIT TRANSACTION;")
+outputfile1.write(" ")
+outputfile2.write("\nCOMMIT TRANSACTION;")
+outputfile2.write(" ")
+
