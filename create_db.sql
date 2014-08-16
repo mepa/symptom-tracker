3670e6b5e9809c89ad8ad86673868fcbb0726ea8
Meghann Agarwal <meg@meghannpalmer.com>
2014-08-16 15:04:43 -0500

    Updated database files

--- create_db.sql
+++ create_db.sql
@@ -1,2 +1,2 @@
 CREATE TABLE user(user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, firstname VARCHAR, dob DATE);
-CREATE TABLE event(event_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, user_id INTEGER, date DATE, start_time TIME, end_time TIME, body_part VARCHAR, symptom VARCHAR, severity INTEGER, temperature FLOAT);
+CREATE TABLE event(event_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, user_id INTEGER, date DATE, start_time TIMESTAMP, end_time TIMESTAMP, body_part VARCHAR, symptom VARCHAR, intensity INTEGER, temperature FLOAT);
