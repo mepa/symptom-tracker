e47f7403dff66f755df061b1596e24be0eda9d0c
Tania Goswami <tania@lp-3621.ad.datafoundry.com>
2014-08-17 11:06:14 -0500

    Added radio buttons for new symptom page

--- SymptomTracker/gen/com/ladieshack/symptomtracker/R.java
+++ SymptomTracker/gen/com/ladieshack/symptomtracker/R.java
@@ -25,10 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000b;
         public static final int birthday_day_input=0x7f080006;
         public static final int birthday_month_input=0x7f080005;
         public static final int birthday_year_input=0x7f080007;
+        public static final int choose_foot=0x7f08000a;
+        public static final int choose_head=0x7f080009;
         public static final int go_to_main_menu_button=0x7f080008;
         public static final int go_to_open_symptom_button=0x7f080002;
         public static final int go_to_report_symptoms_button=0x7f080003;
@@ -48,11 +50,13 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050010;
+        public static final int action_settings=0x7f050012;
         public static final int app_name=0x7f050000;
         public static final int dd=0x7f050008;
         public static final int dob=0x7f050006;
         public static final int enter_app=0x7f050003;
+        public static final int foot=0x7f050011;
+        public static final int head=0x7f050010;
         public static final int hello_world=0x7f050001;
         /**  Main menu screen 
          */
--- SymptomTracker/src/com/ladieshack/symptomtracker/WholeBodyActivity.java
+++ SymptomTracker/src/com/ladieshack/symptomtracker/WholeBodyActivity.java
@@ -2,6 +2,8 @@ package com.ladieshack.symptomtracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class WholeBodyActivity extends Activity {
 
@@ -9,6 +11,23 @@ public class WholeBodyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whole_body);
+    }
+    
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.choose_head:
+                if (checked)
+                    
+                break;
+            case R.id.choose_foot:
+                if (checked)
+                    
+                break;
+        }
     }
 	
 }
